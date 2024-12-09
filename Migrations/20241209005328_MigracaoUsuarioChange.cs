7abcb44a70625e71e39644f893d23509ffb33d12
Atr0fy <2006lucasns@gmail.com>
2024-12-08 21:58:33 -0300

    script banco de dados

---
+++ Migrations/20241209005328_MigracaoUsuarioChange.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TarefasApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracaoUsuarioChange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "TB_USUARIOS",
+                newName: "Username");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Perfil",
+                table: "TB_USUARIOS",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "Jogador",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "TB_USUARIOS",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "PasswordHash", "PasswordSalt" },
+                values: new object[] { new byte[] { 229, 221, 115, 220, 15, 192, 49, 66, 4, 65, 16, 186, 252, 205, 44, 144, 25, 158, 176, 141, 31, 140, 190, 177, 117, 65, 188, 95, 78, 13, 10, 123, 177, 141, 92, 184, 86, 146, 222, 229, 1, 214, 25, 27, 205, 94, 195, 89, 230, 139, 181, 83, 254, 12, 249, 64, 222, 168, 115, 156, 191, 65, 184, 108 }, new byte[] { 233, 83, 210, 194, 8, 157, 52, 130, 28, 103, 60, 228, 178, 115, 184, 190, 254, 18, 107, 253, 116, 53, 229, 114, 180, 0, 28, 218, 122, 160, 31, 182, 137, 146, 176, 100, 82, 210, 86, 102, 124, 15, 102, 87, 119, 164, 20, 158, 150, 208, 165, 93, 253, 250, 187, 209, 183, 134, 63, 137, 145, 23, 98, 110, 219, 174, 20, 149, 234, 171, 188, 132, 26, 207, 93, 111, 172, 233, 148, 79, 98, 212, 63, 90, 111, 163, 168, 161, 204, 16, 68, 3, 83, 232, 120, 218, 179, 131, 128, 99, 31, 127, 90, 42, 232, 234, 95, 224, 125, 43, 205, 83, 4, 135, 228, 162, 103, 150, 30, 224, 81, 168, 10, 208, 124, 181, 28, 6 } });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "TB_USUARIOS",
+                newName: "Nome");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Perfil",
+                table: "TB_USUARIOS",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "Jogador");
+
+            migrationBuilder.UpdateData(
+                table: "TB_USUARIOS",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "PasswordHash", "PasswordSalt" },
+                values: new object[] { new byte[] { 29, 98, 110, 151, 117, 3, 172, 241, 204, 175, 115, 211, 84, 242, 148, 104, 157, 116, 56, 210, 19, 85, 194, 41, 30, 243, 33, 234, 228, 28, 176, 143, 48, 217, 167, 149, 180, 238, 12, 11, 225, 152, 225, 169, 0, 188, 31, 220, 146, 7, 119, 105, 91, 86, 245, 3, 227, 199, 41, 86, 223, 149, 166, 236 }, new byte[] { 80, 24, 103, 88, 105, 154, 47, 223, 178, 134, 185, 104, 25, 252, 145, 138, 70, 100, 93, 78, 85, 126, 86, 240, 18, 36, 26, 65, 189, 237, 104, 155, 232, 65, 197, 12, 56, 126, 170, 108, 135, 226, 44, 38, 183, 95, 137, 4, 158, 208, 178, 239, 66, 108, 70, 13, 94, 156, 218, 180, 181, 196, 212, 30, 173, 59, 127, 50, 18, 193, 83, 17, 148, 12, 77, 29, 112, 23, 171, 68, 21, 2, 152, 215, 190, 15, 201, 240, 35, 108, 156, 160, 141, 29, 85, 57, 179, 224, 4, 86, 135, 67, 219, 63, 25, 226, 117, 42, 197, 135, 123, 176, 231, 142, 115, 247, 160, 66, 163, 252, 229, 87, 78, 109, 76, 59, 61, 35 } });
+        }
+    }
+}
---
+++ estudosapi.sql
@@ -0,0 +1,252 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [TB_TAREFAS] (
+    [Id] int NOT NULL IDENTITY,
+    [Data] datetime2 NOT NULL,
+    [Nome] nvarchar(max) NOT NULL,
+    [Prioridade] int NOT NULL,
+    [Completo] bit NOT NULL,
+    CONSTRAINT [PK_TB_TAREFAS] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241108151157_InitialCreate', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Completo', N'Data', N'Nome', N'Prioridade') AND [object_id] = OBJECT_ID(N'[TB_TAREFAS]'))
+    SET IDENTITY_INSERT [TB_TAREFAS] ON;
+INSERT INTO [TB_TAREFAS] ([Id], [Completo], [Data], [Nome], [Prioridade])
+VALUES (1, CAST(0 AS bit), '2024-11-15T00:00:00.0000000', N'Estudar Matemática', 2),
+(2, CAST(0 AS bit), '2024-11-16T00:00:00.0000000', N'Revisar Física', 1),
+(3, CAST(0 AS bit), '2024-11-17T00:00:00.0000000', N'Ler sobre Química', 0);
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Completo', N'Data', N'Nome', N'Prioridade') AND [object_id] = OBJECT_ID(N'[TB_TAREFAS]'))
+    SET IDENTITY_INSERT [TB_TAREFAS] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241108151911_TarefasExemplo', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+EXEC sp_rename N'[TB_TAREFAS].[Data]', N'DataTermino', N'COLUMN';
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241110002413_dataTermino', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+DECLARE @var0 sysname;
+SELECT @var0 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[TB_TAREFAS]') AND [c].[name] = N'DataTermino');
+IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [TB_TAREFAS] DROP CONSTRAINT [' + @var0 + '];');
+ALTER TABLE [TB_TAREFAS] ALTER COLUMN [DataTermino] nvarchar(max) NOT NULL;
+GO
+
+UPDATE [TB_TAREFAS] SET [DataTermino] = N'05/10'
+WHERE [Id] = 1;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [TB_TAREFAS] SET [DataTermino] = N'11/10'
+WHERE [Id] = 2;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [TB_TAREFAS] SET [DataTermino] = N'28/11'
+WHERE [Id] = 3;
+SELECT @@ROWCOUNT;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241110003200_stringDataTermino', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [TB_CATEGORIAS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_TB_CATEGORIAS] PRIMARY KEY ([Id])
+);
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_CATEGORIAS]'))
+    SET IDENTITY_INSERT [TB_CATEGORIAS] ON;
+INSERT INTO [TB_CATEGORIAS] ([Id], [Nome])
+VALUES (1, N'Matematica');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_CATEGORIAS]'))
+    SET IDENTITY_INSERT [TB_CATEGORIAS] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241110005903_categoriasExemplo', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [TB_TAREFAS] ADD [CategoriaId] int NOT NULL DEFAULT 0;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_CATEGORIAS]'))
+    SET IDENTITY_INSERT [TB_CATEGORIAS] ON;
+INSERT INTO [TB_CATEGORIAS] ([Id], [Nome])
+VALUES (2, N'Fisica');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_CATEGORIAS]'))
+    SET IDENTITY_INSERT [TB_CATEGORIAS] OFF;
+GO
+
+UPDATE [TB_TAREFAS] SET [CategoriaId] = 1
+WHERE [Id] = 1;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [TB_TAREFAS] SET [CategoriaId] = 2
+WHERE [Id] = 2;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [TB_TAREFAS] SET [CategoriaId] = 1, [Nome] = N'Regra de três'
+WHERE [Id] = 3;
+SELECT @@ROWCOUNT;
+
+GO
+
+CREATE INDEX [IX_TB_TAREFAS_CategoriaId] ON [TB_TAREFAS] ([CategoriaId]);
+GO
+
+ALTER TABLE [TB_TAREFAS] ADD CONSTRAINT [FK_TB_TAREFAS_TB_CATEGORIAS_CategoriaId] FOREIGN KEY ([CategoriaId]) REFERENCES [TB_CATEGORIAS] ([Id]) ON DELETE CASCADE;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241111005952_categoriaRelacaoTarefa', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+ALTER TABLE [TB_CATEGORIAS] ADD [UsuarioId] int NULL;
+GO
+
+CREATE TABLE [TB_USUARIOS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NOT NULL,
+    [PasswordHash] varbinary(max) NULL,
+    [PasswordSalt] varbinary(max) NULL,
+    [Foto] varbinary(max) NULL,
+    [Latitude] float NULL,
+    [Longitude] float NULL,
+    [DataAcesso] datetime2 NULL,
+    [Perfil] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NULL,
+    CONSTRAINT [PK_TB_USUARIOS] PRIMARY KEY ([Id])
+);
+GO
+
+UPDATE [TB_CATEGORIAS] SET [UsuarioId] = NULL
+WHERE [Id] = 1;
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [TB_CATEGORIAS] SET [UsuarioId] = NULL
+WHERE [Id] = 2;
+SELECT @@ROWCOUNT;
+
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'DataAcesso', N'Email', N'Foto', N'Latitude', N'Longitude', N'Nome', N'PasswordHash', N'PasswordSalt', N'Perfil') AND [object_id] = OBJECT_ID(N'[TB_USUARIOS]'))
+    SET IDENTITY_INSERT [TB_USUARIOS] ON;
+INSERT INTO [TB_USUARIOS] ([Id], [DataAcesso], [Email], [Foto], [Latitude], [Longitude], [Nome], [PasswordHash], [PasswordSalt], [Perfil])
+VALUES (1, NULL, N'seuEmail@gmail.com', NULL, -23.520024100000001E0, -46.596497999999997E0, N'UsuarioAdmin', 0x1D626E977503ACF1CCAF73D354F294689D7438D21355C2291EF321EAE41CB08F30D9A795B4EE0C0BE198E1A900BC1FDC920777695B56F503E3C72956DF95A6EC, 0x50186758699A2FDFB286B96819FC918A46645D4E557E56F012241A41BDED689BE841C50C387EAA6C87E22C26B75F89049ED0B2EF426C460D5E9CDAB4B5C4D41EAD3B7F3212C15311940C4D1D7017AB44150298D7BE0FC9F0236C9CA08D1D5539B3E004568743DB3F19E2752AC5877BB0E78E73F7A042A3FCE5574E6D4C3B3D23, N'Admin');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'DataAcesso', N'Email', N'Foto', N'Latitude', N'Longitude', N'Nome', N'PasswordHash', N'PasswordSalt', N'Perfil') AND [object_id] = OBJECT_ID(N'[TB_USUARIOS]'))
+    SET IDENTITY_INSERT [TB_USUARIOS] OFF;
+GO
+
+CREATE INDEX [IX_TB_CATEGORIAS_UsuarioId] ON [TB_CATEGORIAS] ([UsuarioId]);
+GO
+
+ALTER TABLE [TB_CATEGORIAS] ADD CONSTRAINT [FK_TB_CATEGORIAS_TB_USUARIOS_UsuarioId] FOREIGN KEY ([UsuarioId]) REFERENCES [TB_USUARIOS] ([Id]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241114005708_Usuario', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+EXEC sp_rename N'[TB_USUARIOS].[Nome]', N'Username', N'COLUMN';
+GO
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[TB_USUARIOS]') AND [c].[name] = N'Perfil');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [TB_USUARIOS] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [TB_USUARIOS] ADD DEFAULT N'Jogador' FOR [Perfil];
+GO
+
+UPDATE [TB_USUARIOS] SET [PasswordHash] = 0xE5DD73DC0FC03142044110BAFCCD2C90199EB08D1F8CBEB17541BC5F4E0D0A7BB18D5CB85692DEE501D6191BCD5EC359E68BB553FE0CF940DEA8739CBF41B86C, [PasswordSalt] = 0xE953D2C2089D34821C673CE4B273B8BEFE126BFD7435E572B4001CDA7AA01FB68992B06452D256667C0F665777A4149E96D0A55DFDFABBD1B7863F899117626EDBAE1495EAABBC841ACF5D6FACE9944F62D43F5A6FA3A8A1CC10440353E878DAB38380631F7F5A2AE8EA5FE07D2BCD530487E4A267961EE051A80AD07CB51C06
+WHERE [Id] = 1;
+SELECT @@ROWCOUNT;
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20241209005328_MigracaoUsuarioChange', N'8.0.10');
+GO
+
+COMMIT;
+GO
+
