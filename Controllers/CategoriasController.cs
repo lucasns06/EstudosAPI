96e34bb24404d3ed4e213e25a6fbadd2fbfd350f
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-01-18 13:36:30 -0300

    teste de autenticação

--- Controllers/CategoriasController.cs
+++ Controllers/CategoriasController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -19,18 +20,40 @@ namespace TarefasApi.Controllers
         {
             _context = context;
         }
-        /// <summary>
-        /// testando 
-        /// </summary>
-        /// <returns>testando 2</returns>
+        // [HttpGet("GetAll")]
+        // public async Task<IActionResult> GetAll()
+        // {
+        //     try
+        //     {
+        //         List<Categoria> categorias = await _context.TB_CATEGORIAS
+        //         .Include(t => t.Tarefas)
+        //         .ToListAsync();
+        //         return Ok(categorias);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return BadRequest(ex.Message + " - " + ex.InnerException);
+        //     }
+        // }
         [HttpGet("GetAll")]
         public async Task<IActionResult> GetAll()
         {
             try
             {
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+                if (userId == null)
+                {
+                    return Unauthorized("Usuário não autenticado.");
+                }
+
+                int userIdInt = Convert.ToInt32(userId);
+
                 List<Categoria> categorias = await _context.TB_CATEGORIAS
-                .Include(t => t.Tarefas)
-                .ToListAsync();
+                    .Where(c => c.UsuarioId == userIdInt)
+                    .Include(t => t.Tarefas)
+                    .ToListAsync();
+
                 return Ok(categorias);
             }
             catch (Exception ex)
@@ -38,6 +61,7 @@ namespace TarefasApi.Controllers
                 return BadRequest(ex.Message + " - " + ex.InnerException);
             }
         }
+
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
@@ -61,19 +85,46 @@ namespace TarefasApi.Controllers
                 return BadRequest(ex.Message + " - " + ex.InnerException);
             }
         }
-        [HttpPost]
+        // [HttpPost]
+        // public async Task<IActionResult> PostCategoria(Categoria categoria)
+        // {
+        //     try
+        //     {
+        //         if (categoria != null)
+        //         {
+        //             categoria.Tarefas = null;
+
+        //             await _context.TB_CATEGORIAS.AddAsync(categoria);
+        //             await _context.SaveChangesAsync();
+
+        //             return Ok(categoria);
+        //         }
+        //         else
+        //         {
+        //             return BadRequest("Categoria inválida");
+        //         }
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return BadRequest(ex.Message + " - " + ex.InnerException);
+        //     }
+        // }
         public async Task<IActionResult> PostCategoria(Categoria categoria)
         {
             try
             {
                 if (categoria != null)
                 {
+                    var usuarioId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+                    categoria.UsuarioId = usuarioId;
+
                     categoria.Tarefas = null;
-                     
+
                     await _context.TB_CATEGORIAS.AddAsync(categoria);
                     await _context.SaveChangesAsync();
 
-                    return Ok(categoria); 
+                    return Ok(categoria);
                 }
                 else
                 {
